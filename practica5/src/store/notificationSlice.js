e65a0bdc1c78df80b7a3526cec55886f97990d6a
danielaguardad0 <danielamguatdado@gmail.com>
2025-03-16 12:41:32 -0600

    1

--- practica5/src/store/notificationSlice.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const useNotificationStore = create((set) => ({
-    notification: null,
-    showNotification: (message, type = 'success') => set({ notification: { message, type } }),
-    hideNotification: () => set({ notification: null }),
-}));
--- practica5/src/store/useAppStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware";
-import { createRecipesSlice } from "./recipeSlice";
-import { createFavoritesSlice } from "./favoritesSlice";
-
-export const useAppStore = create(devtools((...args) => ({
-    ...createRecipesSlice(...args),
-    ...createFavoritesSlice(...args)
-})))
